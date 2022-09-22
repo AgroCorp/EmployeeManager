39da1bacff66eee99fd45d88713fec8ace473b39
Gabor Horvath <gaborka812@gmail.com>
2022-09-22 16:01:59 +0200

    Add angular front end TODO CORS

---
+++ employeemanagerapp/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {Employee} from "./employee";
+import {EmployeeService} from "./employee.service";
+import {HttpErrorResponse} from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  public employees: Employee[] | undefined;
+  title: string;
+
+  constructor(private employeeService: EmployeeService) {
+    this.title = "employeemanagerapp";
+  }
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+
+}
